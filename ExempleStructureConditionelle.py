d5d0a82b3b54de7d1adb5ac8f3a043681cf573fc
Leo Lhomme <leo.lhomme@labom2iformation.fr>
2025-02-07 14:51:35 +0100

    Python_algo _ demo structure conditionelle

---
+++ ExempleStructureConditionelle.py
@@ -0,0 +1,9 @@
+age = int(input("Entrer votre age : "))
+
+
+if age >= 21 :
+    print("Vous etes majeur au USA !")
+elif age >= 18 :
+    print("Vous etes majeu en europe mais pas au USA")
+else :
+    print("Vous etes mineur")
